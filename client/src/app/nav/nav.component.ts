f5b6c5d392b2a020c46581efe4da17c5cb40a587
Shaimaa <shyaali98@gmail.com>
2024-09-15 11:13:41 +0300

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,30 +3,35 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, RouterLinkActive, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule/*, NgIf*/,BsDropdownModule],
+  imports: [FormsModule/*, NgIf*/,BsDropdownModule, RouterModule, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
+        this.router.navigateByUrl("/members");
       },
-      error: error => console.log(error),
-      complete: () => console.log("done"),
+      error: error => this.toastr.error(error.error),
     });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -21,6 +22,7 @@ export class RegisterComponent {
   private accountService = inject(AccountService);
   // usersFromHomeComponent = input.required<any>();
   /*@Output()*/ cancelRegister = output<boolean>() /*new EventEmitter()*/;
+  private toastr = inject(ToastrService);
   model: any = {};
 
   register() {
@@ -28,8 +30,8 @@ export class RegisterComponent {
       next: (response) => {
         console.log(response);
       },
-      error: (error) => console.log(error),
-      complete: () => console.log('done'),
+      error: (error) => this.toastr.error(error.error),
+      complete: () => this.toastr.success("Registration done successfuly"),
     });
   }
 
